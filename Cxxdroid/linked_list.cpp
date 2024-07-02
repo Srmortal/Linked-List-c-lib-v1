6ca19db6303127e582aa0cc7d6ffd52e7b1b441b
Srmortal <166674803+Srmortal@users.noreply.github.com>
2024-07-02 09:12:54 +0300

    Update linked_list.cpp

--- Cxxdroid/linked_list.cpp
+++ Cxxdroid/linked_list.cpp
@@ -1,29 +1,64 @@
 #ifndef LINKED_LIST1
 #define LINKED_LIST1
-#include "put path of linked_list.h"
-template<class T>
-T LinkedList<T>::operator[](unsigned i){
-    Node<T>* n = head;
+#include "C:\Users\YOUSEF\OneDrive\Desktop\Cxxdroid\linked test.h"
+template<class t, linked_list_type LT>
+linked_list_iterator<t,LT>& linked_list_iterator<t,LT>::operator++()
+{
+    if (Node)
+    {
+        this->Node = this->Node->next;
+    }
+    return *this;
+}
+template<class t, linked_list_type LT>
+linked_list_iterator<t, LT> linked_list_iterator<t,LT>::operator++(int)
+{
+    linked_list_iterator<t,LT> temp = *this;
+    ++(*this);
+    return temp;
+}
+template<class T, linked_list_type LT>
+linked_list_iterator<T, linked_list_type::doubly>& linked_list_iterator<T, LT>::operator--()
+{
+    static_assert(LT == linked_list_type::doubly, "decrement is only supported for doubly linked lists.");
+    if (this->Node) {
+        this->Node = this->Node->prev;
+    }
+    return *this;
+}
+template<class T, linked_list_type LT>
+linked_list_iterator<T, linked_list_type::doubly> linked_list_iterator<T, LT>::operator--(int)
+{
+    static_assert(LT == linked_list_type::doubly, "decrement is only supported for doubly linked lists.");
+    linked_list_iterator<T, LT> temp = *this;
+    --(*this);
+    return temp;
+}
+template<class T, linked_list_type LT>
+T& linked_list_iterator<T,LT>::operator*()
+{
+    return this->Node->data;
+}
+template<class T,linked_list_type LT>
+bool linked_list_iterator<T, LT>::operator!=(const linked_list_iterator iter)
+{
+    return this->Node != iter.Node;
+}
+template<class T,linked_list_type LT>
+T& linked_list<T,LT>::operator[](unsigned i){
+    linked_list<T, LT>::node* n = head;
     while (i && n) {
         n = n->next;
         i--;
     }
+    if (n == nullptr || i >= size()) {
+        throw std::out_of_range("out of bounds");
+    }
     return n->data;
 }
-template<class T>
-ostream& operator<<(ostream& COUT, Node<T>& node)
-{
-    COUT << node.data;
-    return COUT;
-}
-template<class T>
-T& Node<T>::operator*()
-{
-    return data;
-}
-template<class T>
-int LinkedList<T>::search(T c){
-    Node<T> *x=head;
+template<class T,linked_list_type LT>
+int linked_list<T,LT>::search_by_index(T c){
+    linked_list<T, LT>::node *x=head;
     int i=0;
     while(x){
         if(x->data==c){
@@ -34,24 +69,37 @@ int LinkedList<T>::search(T c){
     }
     return -1;
 }
-template<class T>
-Node<T>* LinkedList<T>::operator+(int index)
+template<class T,linked_list_type LT>
+int linked_list<T,LT>::search_by_index(std::function<bool(const T&)> func)
 {
-    Node<T>* n = head;
-    if (!index)
+    linked_list<T, LT>::node* it = head;
+    int i = 0;
+    while (it != nullptr)
     {
-        return head;
+        if (func(it->data)) {
+            return i;
+        }
+        i++;
+        it = it->next;
     }
-    int pos = index - 1;
-    while (pos && n) {
-        n = n->next;
-        pos--;
-    }
-    return n;
+    return -1;
 }
-template<class T>
-Node<T> *LinkedList<T>::searchptr(T c){
-    Node<T> *x=head;
+template<class T,linked_list_type LT>
+linked_list_iterator<T,LT> linked_list<T,LT>::search(std::function<bool(const T&)> func)
+{
+    linked_list<T, LT>::node* it = head;
+    while (it!=nullptr)
+    {
+        if (func(it->data)) {
+            return it;
+        }
+        it = it->next;
+    }
+    return nullptr;
+}
+template<class T, linked_list_type LT>
+linked_list_iterator<T, LT> linked_list<T, LT>::search(T c){
+    linked_list<T, LT>::node *x=head;
     while(x){
         if(x->data==c){
             return x;
@@ -60,71 +108,48 @@ Node<T> *LinkedList<T>::searchptr(T c){
     }
     return nullptr;
 }
-template<class T>
-bool LinkedList<T>::is_found(T val)
+template<class T,linked_list_type LT>
+size_t linked_list<T,LT>::size()
 {
-    return search(val)!=-1;
+    return is_empty()? sz:sz+1;
 }
-template<class T>
-Node<T> *LinkedList<T>::headgetter(){
-    return head;
-}
-template<class T>
-Node<T> *LinkedList<T>::tailgetter(){
-    Node<T> *x=head;
-    if(is_empty()) return NULL;
-    while(x->next){
-        x=x->next;
-    }
-    return x;
-}
-template<class T>
-int LinkedList<T>::size()
-{
-    int cnt = 0;
-    Node<T>* it = head;
-    while (it)
+template<class T,linked_list_type LT>
+void linked_list<T,LT>::push_front(const T c){
+    linked_list<T, LT>::node* n = new linked_list<T, LT>::node(c);
+    if (is_empty()) { tail = head = n; return; }
+    n->next = head;
+    if (LT==linked_list_type::doubly)
     {
-        cnt++;
-        it = it->next;
+        head->prev = n;
     }
-    return cnt;
+    head = n;
+    sz++;
 }
-template<class T>
-unsigned LinkedList<T>::sizeno()
-{
-    return sz;
-}
-template<class T>
-void LinkedList<T>::insertfront1(const T c){
-    Node<T> *n=new Node<T>(c);
-    n->next=head;
-    head=n;
-}
-template<class T>
-void LinkedList<T>::insertend1(const T c){
-    Node<T> *n=new Node<T>(c);
+template<class T, linked_list_type LT>
+void linked_list<T, LT>::push_back(const T c){
     if(is_empty()){
-        head=n;
+        push_front(c);
+        return;
     }
-    else{
-        Node<T> *n1=head;
-        while(n1->next){
-            n1=n1->next;
-        }
-        n1->next=n;
+    linked_list<T, LT>::node* n = new linked_list<T, LT>::node(c);
+    tail->next = n;
+    if (LT == linked_list_type::doubly)
+    {
+        n->prev = tail;
     }
+    tail = n;
+    sz++;
 }
-template<class T>
-bool LinkedList<T>::is_empty()
+template<class T,linked_list_type LT>
+bool linked_list<T,LT>::is_empty()
 {
     return !head;
 }
-template<class T>
-vector<T> LinkedList<T>::to_vector()
+template<class T, linked_list_type LT>
+std::vector<T> linked_list<T, LT>::to_vector()
 {
-    vector<T> res;
-    Node<T>* it = head;
+    std::vector<T> res;
+    linked_list<T, LT>::node* it = head;
     while (it)
     {
         res.push_back(it->data);
@@ -132,25 +157,25 @@ vector<T> LinkedList<T>::to_vector()
     }
     return res;
 }
-template<class T>
-string LinkedList<T>::to_string()
+template<class T, linked_list_type LT>
+std::string linked_list<T, LT>::to_string()
 {
-    stringstream ss;
-    Node<T>* it = head;
+    std::stringstream ss;
+    linked_list<T, LT>::node* it = head;
     while (it)
     {
         ss << it->data;
         it = it->next;
     }
-    string s;
+    std::string s;
     ss >> s;
     return s;
 }
-template<class T>
-stack<T> LinkedList<T>::to_stack()
+template<class T, linked_list_type LT>
+std::stack<T> linked_list<T, LT>::to_stack()
 {
-    stack<T> res;
-    Node<T>* it = head;
+    std::stack<T> res;
+    linked_list<T, LT>::node* it = head;
     while (it)
     {
         res.push(it->data);
@@ -158,11 +183,11 @@ stack<T> LinkedList<T>::to_stack()
     }
     return res;
 }
-template<class T>
-queue<T> LinkedList<T>::to_queue()
+template<class T, linked_list_type LT>
+std::queue<T> linked_list<T, LT>::to_queue()
 {
-    queue<T> res;
-    Node<T>* it = head;
+    std::queue<T> res;
+    linked_list<T, LT>::node* it = head;
     while (it)
     {
         res.push(it->data);
@@ -170,11 +195,11 @@ queue<T> LinkedList<T>::to_queue()
     }
     return res;
 }
-template<class T>
-deque<T> LinkedList<T>::to_deque()
+template<class T, linked_list_type LT>
+std::deque<T> linked_list<T, LT>::to_deque()
 {
-    deque<T> res;
-    Node<T>* it = head;
+    std::deque<T> res;
+    linked_list<T, LT>::node* it = head;
     while (it)
     {
         res.push_back(it->data);
@@ -182,211 +207,162 @@ deque<T> LinkedList<T>::to_deque()
     }
     return res;
 }
-template<class T>
-void LinkedList<T>::clear()
+template<class T, linked_list_type LT>
+void linked_list<T, LT>::reverse()
 {
-    Node<T>* tail = tailgetter();
-    while (head != nullptr) {
-        Node<T>* temp = head;
-        head = head->next;
-        delete temp;
+    if (sz <= 1) {
+        return;
     }
-    tail = nullptr;
+    linked_list<T, LT>::node* current = head;
+    linked_list<T, LT>::node* prevNode = nullptr;
+    linked_list<T, LT>::node* nextNode = nullptr;
+    std::swap(head, tail);
+    while (current != nullptr) {
+        nextNode = current->next;
+        current->next = prevNode;
+        if (LT==linked_list_type::doubly)
+        {
+            current->prev = nextNode;
+        }
+        prevNode = current;
+        current = nextNode;
+    }
+}
+template<class T, linked_list_type LT>
+void linked_list<T, LT>::clear()
+{
+    linked_list<T, LT>::node* current = head;
+    while (current != nullptr) {
+        linked_list<T, LT>::node* next = current->next;
+        delete current;
+        current = next;
+    }
+    tail = head = nullptr;
     sz = 0;
 }
-template<class T>
-void LinkedList<T>::insert1(const T c,unsigned index)
+template<class T, linked_list_type LT>
+void linked_list<T, LT>::insert(const T c,unsigned index)
 {
-    Node<T> *p=head;
-    if (is_empty())
+    if (is_empty()||index==0)
     {
-        insertfront1(c);
+        push_front(c);
         return;
     }
-    if (index==size()-1)
+    if (index==sz)
     {
-        insertend1(c);
+        push_back(c);
         return;
     }
+    linked_list<T, LT>::node* p=head;
     while(p->next&&index){
         p=p->next;
         index--;
     }
-    Node<T> *n = new Node<T>(c);
+    linked_list<T, LT>::node* n = new linked_list<T, LT>::node(c);
     n->next = p->next;
     p->next = n;
-}
-template<class T>
-void LinkedList<T>::insert1(const T c)
-{
-    insert1(c, size() - 1);
+    if (LT==linked_list_type::doubly)
+    {
+        if (n->next) {
+            n->next->prev = n;
+        }
+        n->prev = p;
+    }
     sz++;
 }
-template<class T>
-void LinkedList<T>::removefirst1()
+template<class T, linked_list_type LT>
+void linked_list<T, LT>::pop_front()
 {
-    if (is_empty()){
-       cout<<"no element \n";
-       return;
+    if (is_empty())
+    {
+        throw std::logic_error("List is empty");
     }
-    Node<T> *n = head;
+    linked_list<T, LT>::node* n = head;
     head = head->next;
     delete n;
-}
-template<class T>
-void LinkedList<T>::removelast1()
-{
-    remove1(size() - 1);
-}
-template<class T>
-void LinkedList<T>::removeafterpos1(unsigned index)
-{
-    T c = operator[](index);
-    Node<T> *d= searchptr(c);
-    Node<T> *s=head;
-    if(!d){
-        return;
-    }
-    while(s!=d){
-        s=s->next;
-    }
-    d = s->next;
-    s->next = d->next;
-    delete d;
-}
-template<class T>
-void LinkedList<T>::remove1(unsigned index){
-    sz--;
-    if (index==0)
+    if (LT==linked_list_type::doubly)
     {
-        removefirst1();
-        return;
+        head->prev = nullptr;
     }
-    if (index==size()-1)
-    {
-        removelast1();
-    }
-    removeafterpos1(index-1);
-}
-template<class T>
-void LinkedList<T>::removebyvalue1(T val)
-{
-    int pos = search(val);
-    if (!is_found(val))
-    {
-        cout << "not found";
-        return;
-    }
-    remove1(pos);
-}
-template<class T>
-void LinkedList<T>::insertfront2(const T c){
-    Node<T> *n=new Node<T>(c);
-    if(!head){head=n; return;}
-    head->prev=head;
-    n->next=head;
-    head=n;
-}
-template<class T>
-void LinkedList<T>::insert2(const T o,unsigned index)
-{
-    Node<T> *node = new Node<T>(o);
-    Node<T> *s=head;
-    if (is_empty())
-    {
-        insertfront2(o);
-        return;
-    }
-    if (index==size()-1)
-    {
-        insertend2(o);
-        return;
-    }
-    while(s->next&&index){
-        s=s->next;
-        index--;
-    }
-    node->next = s->next;
-    s->next = node;
-    node->prev=s;
-}
-template<class T>
-void LinkedList<T>::insert2(const T o)
-{
-    insert2(o, size() - 1);
-    sz++;
-}
-template<class T>
-void LinkedList<T>::insertend2(const T o)
-{
-    Node<T> *node = new Node<T>(o);
-    Node<T> *s=tailgetter();
-    if(!head){head=node; return;}
-    s->next = node;
-    node->prev=s;
-}
-template<class T>
-void LinkedList<T>::removefirst2(){
-    if(!head){cout<<"no elements \n"; return;}
-    Node<T> *s=head;
-    head=head->next;
-    free(s);
-    head->prev=NULL;
-}
-template<class T>
-void LinkedList<T>::remove2(unsigned index)
-{
-    if (is_empty())
-    {
-        cout << "no elements \n";
-        return;
-    }
-    if (index == 0)
-    {
-        removefirst2();
-        return;
-    }
-    if (index==size()-1)
-    {
-        removelast2();
-    }
-    Node<T>* s = head;
-    T c = operator[](index);
-    Node<T>* node = searchptr(c);
-    if (!node)
-    {
-        return;
-    }
-    while (s->next != node) {
-        s = s->next;
-    }
-    s->next = node->next;
-    s = node->prev;
-    free(node);
     sz--;
 }
-template<class T>
-void LinkedList<T>::removebyvalue2(const T val)
+template<class T, linked_list_type LT>
+void linked_list<T, LT>::pop_back()
 {
-    int pos = search(val);
-    if (!is_found(val))
-    {
-        cout << "not found\n";
-        return;
-    }
-    remove2(pos);
-}
-template<class T>
-void LinkedList<T>::removelast2()
-{
-    Node<T>* tail = tailgetter();
-    Node<T>* it = head;
-    while (it->next!=tail)
+    linked_list<T, LT>::node* tail1 = tail;
+    linked_list<T, LT>::node* it = head;
+    while (it->next != tail1)
     {
         it = it->next;
     }
     it->next = nullptr;
-    tail->prev = nullptr;
-    delete tail;
+    if (LT==linked_list_type::doubly)
+    {
+        tail1->prev = nullptr;
+    }
+    delete tail1;
+    tail = it;
+    sz--;
+}
+template<class T, linked_list_type LT>
+void linked_list<T, LT>::erase(linked_list_iterator<T, LT> it){
+    if (is_empty())
+    {
+        throw std::logic_error("List is empty");
+    }
+    if (it == head)
+    {
+        pop_front();
+        return;
+    }
+    if (it == tail)
+    {
+        pop_back();
+        return;
+    }
+    linked_list<T, LT>::node* s = head;
+    if (it==end())
+    {
+        throw std::logic_error("element is not found");
+    }
+    while (s->next != it) {
+        s = s->next;
+    }
+    node* e = it;
+    s->next = e->next;
+    if (LT==linked_list_type::doubly)
+    {
+        s = e->prev;
+    }
+    delete it;
+    sz--;
+}
+template<class T, linked_list_type LT>
+void linked_list<T, LT>::remove(T val)
+{
+    node* pos = search(val);
+    erase(pos);
+}
+template<class T, linked_list_type LT>
+linked_list_iterator<T, LT> linked_list<T, LT>::begin() const
+{
+    return linked_list_iterator<T,LT>(head);
+}
+template<class T, linked_list_type LT>
+linked_list_iterator<T, LT> linked_list<T, LT>::end() const
+{
+    return linked_list_iterator<T,LT>(nullptr);
+}
+template<class T, linked_list_type LT>
+linked_list_iterator<T, linked_list_type::doubly> linked_list<T, LT>::rbegin() const
+{
+    static_assert(LT == linked_list_type::doubly, "rend() is only supported for doubly linked lists.");
+    return linked_list_iterator<T, linked_list_type::doubly>(tail);
+}
+template<class T, linked_list_type LT>
+linked_list_iterator<T, linked_list_type::doubly> linked_list<T, LT>::rend() const
+{
+    static_assert(LT == linked_list_type::doubly, "rend() is only supported for doubly linked lists.");
+    return linked_list_iterator<T, linked_list_type::doubly>(nullptr);
 }
 #endif //LINKED_LIST1
